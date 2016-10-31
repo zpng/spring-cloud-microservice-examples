a36f713f2bc31f0924421cd7a25644f4f4f79b27
zhangpeng <zhangpeng@fenbi.com>
2016-10-31 20:01:09 +0800

    MOD: enable hystrix turbine stream amqp

--- cloud-hystrix-turbine/src/main/java/cloud/hystrix/turbine/HystrixTurbine.java
+++ cloud-hystrix-turbine/src/main/java/cloud/hystrix/turbine/HystrixTurbine.java
@@ -10,8 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * @author zhangpeng
@@ -19,9 +18,8 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrixDashboard
-@EnableTurbine
 @EnableDiscoveryClient
+@EnableTurbineStream
 public class HystrixTurbine {
 
     public static void main(String[] args) {
