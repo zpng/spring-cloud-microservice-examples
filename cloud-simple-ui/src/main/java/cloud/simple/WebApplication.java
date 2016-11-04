484660cc04670e59d35b26443be443e3b140288d
zhangpeng <zhangpeng@fenbi.com>
2016-11-04 19:24:13 +0800

    MOD: add robbin

--- cloud-simple-ui/src/main/java/cloud/simple/WebApplication.java
+++ cloud-simple-ui/src/main/java/cloud/simple/WebApplication.java
@@ -6,26 +6,21 @@
  */
 package cloud.simple;
 
-import feign.Feign;
-import feign.Logger;
-import feign.Request;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zhangpeng
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableFeignClients
+//@EnableFeignClients
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
@@ -38,22 +33,22 @@ public class WebApplication {
         return new RestTemplate();
     }
 
-    @Bean
-    @Scope("prototype")
-    public Feign.Builder feignBuilder() {
-        return Feign.builder();
-    }
-
-    @Bean
-    public Logger.Level feignLogger() {
-        return Logger.Level.FULL;
-    }
-
-    private static final int FIVE_SECONDS = 5000;
-
-    @Bean
-    public Request.Options options() {
-        return new Request.Options(FIVE_SECONDS, FIVE_SECONDS);
-    }
+//    @Bean
+//    @Scope("prototype")
+//    public Feign.Builder feignBuilder() {
+//        return Feign.builder();
+//    }
+//
+//    @Bean
+//    public Logger.Level feignLogger() {
+//        return Logger.Level.FULL;
+//    }
+//
+//    private static final int FIVE_SECONDS = 5000;
+//
+//    @Bean
+//    public Request.Options options() {
+//        return new Request.Options(FIVE_SECONDS, FIVE_SECONDS);
+//    }
 
 }
